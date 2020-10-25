fb9aa0d004264724eced807a7ee227ef6442cfd5
DARKxKNIGHTx <73416366+DARKxKNIGHTx@users.noreply.github.com>
2020-10-25 19:06:36 +0530

    Add files via upload

---
+++ DDOS-PRANK.py
@@ -0,0 +1,56 @@
+'''
+******************************************************************************** 
+THIS PROGRAM IS EXTREMELY DANGEROUS
+YOU MAY LAND IN TROUBLE IF YOU ARE RUNNING WITHOUT VICTIMS PERMISSION
+THIS IS CREATED BY DARKxKNIGHTx A SO CALLED PROGRAMER
+********************************************************************************
+'''
+import time
+import sys
+time.sleep(3)
+print("You are about to attack a ?? \n	1.SCHOOL WEBSITE\n	2.GOVERMEN WEBSITE\n	3.YOUR OWN WEBSITE")
+opt=raw_input("option number : ")
+time.sleep(3)
+print("Please Wait......")
+time.sleep(5)
+print("\n\tDDOS ATTACK IS LOADING........")
+time.sleep(5)
+url=raw_input("THE URL OF THE WEBSITE = ")
+print("do a nmaping in the website and enter below the open ports.....")
+time.sleep(5)
+port=("\nOPEN PORTS IN THE WEBSITE = ")
+time.sleep(3)
+print("\n\nDDOS ATTACK IS LOADING........")
+time.sleep(8)
+print('''                                       
+                                       .+ooyy-        oysoo:                                        
+                                       .```.-`        .-.``.`                                       
+                                         `+:           `+-                                          
+                                        `+ss:         `oss-                                         
+                                       `+ssss:       `ossss:                                        
+                                       -ssssso`      :sssss+                                        
+                                        :ssso.        /sss+`                                        
+                                         -so`          :s+`                                         
+                                          -`    :ss-    -`                                          
+                                     :-       .ohddh+        :.                                     
+                                      +s-     .syyyyo      -o/                                      
+                                       /hs-`            `.oy:                                       
+                                        .shys+++osso+++syhs.                                        
+                                          -shddddddddddhs:                                          
+                                            .:+syyyyso:.                                            
+                                                 ``     
+''')
+time.sleep(10)
+print("We need to tell you something....")
+time.sleep(4)
+print("That....")
+time.sleep(15)
+print("YOU ARE NOT ELIGIBLE TO CONDUCT A DDOS ATTACK!")
+time.sleep(5)
+print('''
+
+ 				LOL
+
+''')
+time.sleep(60)
+
